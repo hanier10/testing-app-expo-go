3bca60e2e80f384b6ee49abb0df36f89fe7e1673
Hanier Morales <hsmp16@gmail.com>
2025-01-10 21:22:06 -0600

    creating buttons and adding styles

--- App.tsx
+++ App.tsx
@@ -1,10 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import {
+  Pressable,
+  StyleSheet,
+  Text,
+  Touchable,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.textHuge}>{count}</Text>
+
+      <Pressable
+        style={styles.floatingButton}
+        onPress={() => setCount(count + 1)}
+        onLongPress={() => setCount(0)}
+      >
+        <Text style={styles.textButton}>+1</Text>
+      </Pressable>
+
+      <TouchableOpacity>
+        <Text>+1</Text>
+      </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +36,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  textHuge: {
+    fontSize: 120,
+  },
+
+  floatingButton: {
+    position: "absolute",
+    bottom: 20,
+    right: 20,
+    backgroundColor: "#65558F",
+    padding: 20,
+    borderRadius: 15,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.3,
+    elevation: 3,
+    shadowRadius: 4,
+  },
+
+  textButton: {
+    color: "white",
+    fontSize: 20,
   },
 });
